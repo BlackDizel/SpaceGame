981e58ed8255b1ea2e1c8ab786e80727dbb64409
BlackDizel <blackdizel@gmail.com>
2016-02-06 19:48:02 +0300

    add Game Level class stub

---
+++ SpaceGame/Classes/Controller/GameLevel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceGame.Classes.Controller
+{
+    class GameLevel
+    {
+        internal void Init()
+        {
+            //todo implement
+        }
+
+        internal void LoadContent(Microsoft.Xna.Framework.Content.ContentManager Content)
+        {
+            //todo implement
+        }
+
+        internal void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            //todo implement
+        }
+
+        internal void Input(Microsoft.Xna.Framework.PlayerIndex playerIndex, Microsoft.Xna.Framework.GameTime gameTime, Microsoft.Xna.Framework.Input.KeyboardState keystate, Microsoft.Xna.Framework.Input.MouseState mousestate, Microsoft.Xna.Framework.Input.GamePadState gamepadState)
+        {
+            //todo implement
+        }
+
+        internal void Draw(Microsoft.Xna.Framework.GameTime gameTime, Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            //todo implement
+        }
+    }
+}
--- SpaceGame/Game1.cs
+++ SpaceGame/Game1.cs
@@ -1,29 +1,35 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SpaceGame.Classes.Controller;
 
 namespace SpaceGame
 {
 
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private GameLevel level;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            level = new GameLevel();
         }
 
         protected override void Initialize()
         {
             base.Initialize();
+            level.Init();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            level.LoadContent(Content);
         }
 
         protected override void UnloadContent()
@@ -35,12 +41,20 @@ namespace SpaceGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            KeyboardState keystate = Keyboard.GetState();
+            MouseState mousestate = Mouse.GetState();
+            GamePadState gamepadState = GamePad.GetState(PlayerIndex.One);
+            
+            level.Update(gameTime);
+            level.Input(PlayerIndex.One, gameTime, keystate, mousestate, gamepadState);
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            level.Draw(gameTime,spriteBatch);
             base.Draw(gameTime);
         }
     }
